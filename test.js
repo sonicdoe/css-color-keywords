3976e33cfd9134d2278ef213dfd03ff5725d29fb
Jakob Krigovsky <jakob@krigovsky.com>
2017-01-20 18:08:28 +0100

    Add tests

---
+++ test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const assert = require('assert')
+const cssColorKeywords = require('.')
+
+assert.strictEqual(cssColorKeywords.black, '#000000')
+assert.strictEqual(cssColorKeywords.orange, '#ffa500')
